d8c143166fc0a9ba0a075e8f6dc7e318337988c2
miracline2 <francesca4518@gmail.com>
2025-06-01 23:09:25 +0530

    Update path

--- src/common/index.ts
+++ src/common/index.ts
@@ -7,7 +7,7 @@ import typeScript from '../assets/Images/Ts.png';
 import nodeJs from '../assets/Images/nodeJS.png';
 import npm from '../assets/Images/npm.png';
 import vsCode from '../assets/Images/Vscode.png';
-import gitHub from '../assets/Images/github.png';
+import gitHub from '../assets/Images/gitHub.png';
 import azureDevops from '../assets/Images/AzureDevops.png';
 export const navLinks = [
   {
@@ -24,79 +24,6 @@ export const navLinks = [
   },
 ];
 
-// const services = [
-//   {
-//     title: "Web Developer",
-
-//   },
-//   {
-//     title: "React Native Developer",
-
-//   },
-//   {
-//     title: "Backend Developer",
-//     icon: backend,
-//   },
-//   {
-//     title: "Content Creator",
-//     icon: creator,
-//   },
-// ];
-
-// const technologies = [
-//   {
-//     name: "HTML 5",
-//     icon: html,
-//   },
-//   {
-//     name: "CSS 3",
-//     icon: css,
-//   },
-//   {
-//     name: "JavaScript",
-//     icon: javascript,
-//   },
-//   {
-//     name: "TypeScript",
-//     icon: typescript,
-//   },
-//   {
-//     name: "React JS",
-//     icon: reactjs,
-//   },
-//   {
-//     name: "Redux Toolkit",
-//     icon: redux,
-//   },
-//   {
-//     name: "Tailwind CSS",
-//     icon: tailwind,
-//   },
-//   {
-//     name: "Node JS",
-//     icon: nodejs,
-//   },
-//   {
-//     name: "MongoDB",
-//     icon: mongodb,
-//   },
-//   {
-//     name: "Three JS",
-//     icon: threejs,
-//   },
-//   {
-//     name: "git",
-//     icon: git,
-//   },
-//   {
-//     name: "figma",
-//     icon: figma,
-//   },
-//   {
-//     name: "docker",
-//     icon: docker,
-//   },
-// ];
 
 const experiences = [
   {
